86b3768b886577748ce1c6e7ed8b4de5f595a1a4
Courtney-Watts <courtney@siteglide.com>
2020-03-08 14:27:04 +0000

    + default props, style clicked key, play again btn

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Hangmanword from './Hangmanword'
 import Keyboard from './Keyboard'
+import Playagain from './Playagain'
 import '../components/hangman.scss'
 import hangman0 from '../images/hangman0.svg'
 import hangman1 from '../images/hangman1.svg'
@@ -13,23 +14,31 @@ import hangman7 from '../images/hangman7.svg'
 import hangman8 from '../images/hangman8.svg'
 import hangman9 from '../images/hangman9.svg'
 import hangman10 from '../images/hangman10.svg'
+import hangman11 from '../images/hangman11.svg'
+
 
 class Hangman extends Component {
   static defaultProps = {
-    images: [hangman0,hangman1,hangman2,hangman3,hangman4,hangman5,hangman6,hangman7,hangman8,hangman9,hangman10]
+    images: [hangman0,hangman1,hangman2,hangman3,hangman4,hangman5,hangman6,hangman7,hangman8,hangman9,hangman10,hangman11],
+    words: ['Toy Story', 'Shrek', 'Ice Age', 'The Incredibles', 'Wreck It Ralph', 'Frozen', 'How to Train Your Dragon', 'Inside Out', 'Brave', 'Monsters Inc', 'Antz', 'Coco', 'Puss In Boots', 'Kung Fu Panda', 'Tangled', 'Moana', 'Bolt', 'The Lego Movie', 'Finding Dory', 'Zootopia', 'The Adams Family', 'The Secret Life of Pets', 'Trolls', 'Despicable Me', 'The Lion King', 'The Little Mermaid', 'The Nightmare Before Christmas', 'The BFG']
   }
   constructor (props) {
     super(props)
     this.state = {
-      answer: 'Rainbow',
+      answer: 'The Secret Life of Pets',
       incorrectlyGuessed: [],
       guessed: []
 
     }
     this.handleCharacterClick = this.handleCharacterClick.bind(this)
+    this.handlePlayagainClick = this.handlePlayagainClick.bind(this)
   }
 
   handleCharacterClick (event) {
+    if(event.target.classList.contains('clicked')){
+      return
+    }
+    event.target.classList.add('clicked')
     const guessed = event.target.innerText
     if (this.state.answer.toLowerCase().split('').includes(guessed)) {
       console.log('matching')
@@ -43,43 +52,56 @@ class Hangman extends Component {
     }
   }
 
+  handlePlayagainClick(){
+    let idx = Math.floor(Math.random()* this.props.words.length )
+    let nextWord = this.props.words[idx]
+    this.setState({
+      answer: nextWord,
+      guessed:[],
+      incorrectlyGuessed:[]
+    })
+  }
+
   render () {
     const incorrect = this.state.incorrectlyGuessed.length
     const guessedCharacters = this.state.guessed
     const displayedWord = this.state.answer.split('').map(function (char) {
       if (guessedCharacters.includes(char.toLowerCase())) {
         return char
+      } else if (char == ' '){
+        return ''
       } else {
         return '_'
       }
     })
-
-    if(!this.state.gameOver && incorrect >= 7){
-      this.setState({
-        gameOver: true
-      })
-
+    //Game lost ? 
+    let gameLost;
+    if(incorrect > 10){
+      gameLost = true    
     }
-
+    //Game won ? 
+    let gameWon;
     if (!displayedWord.includes('_')){
-      console.log('ya')
+     gameWon = true    
     }
-
-
 
     const wordOnShow = displayedWord.map(function (i, idx) {
-      return <li key={idx}>{i}</li>
+      return <li class={i ? '' : 'space'}key={idx}>{i}</li>
     })
     
     return (
-      <div className='container Hangman'>
+      <div className='container Hangman mt-3'>
         <h1 className='text-center'>Hangman</h1>
         <div className='Hangman-image'>
           <img src={this.props.images[incorrect]} />
         </div>
 
         <Hangmanword word={wordOnShow} />
-        { (!this.state.gameOver) ? <Keyboard onClick={this.handleCharacterClick} /> : <p>gameover</p>}
+        { (gameLost || gameWon) 
+            ?  <h1 className='text-center mt-3'>{gameWon ? 'You Win!' : 'Oh no... you lose'}</h1>
+            : <Keyboard onClick={this.handleCharacterClick} />
+        }
+        { (gameLost || gameWon) && <Playagain onClick={this.handlePlayagainClick}/> }
       </div>
 
     )
