34b9613b01c9081ff2a3a5c11c6dab2abbb316ca
Courtney-Watts <courtney@siteglide.com>
2020-03-08 14:40:46 +0000

    Updates

---
+++ precache-manifest.37f7044ac1d68836bbb2dd4e1c5524b8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aed782b4d5483148ed1fee0d3eae3dd",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "3395b7caf10fa2d5788f",
+    "url": "/hangman/static/css/main.49a5914e.chunk.css"
+  },
+  {
+    "revision": "9a7a55819a87bb231dcf",
+    "url": "/hangman/static/js/2.11aac33e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hangman/static/js/2.11aac33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3395b7caf10fa2d5788f",
+    "url": "/hangman/static/js/main.9a64786c.chunk.js"
+  },
+  {
+    "revision": "faf4ee0c4d3ea0d1992c",
+    "url": "/hangman/static/js/runtime-main.05aa2ca3.js"
+  },
+  {
+    "revision": "6158681b2fa129a43f32fcf4797ce202",
+    "url": "/hangman/static/media/hangman0.6158681b.svg"
+  },
+  {
+    "revision": "7053b647d14d0a6ab7a9ac8f6b49ea3a",
+    "url": "/hangman/static/media/hangman1.7053b647.svg"
+  },
+  {
+    "revision": "74b3bd6dd2658a21da44ffc8d02669cf",
+    "url": "/hangman/static/media/hangman10.74b3bd6d.svg"
+  },
+  {
+    "revision": "33513f5a88ca728d8da4fe680364a520",
+    "url": "/hangman/static/media/hangman11.33513f5a.svg"
+  },
+  {
+    "revision": "ab2a09a0f74c5bbac35071b201f615db",
+    "url": "/hangman/static/media/hangman2.ab2a09a0.svg"
+  },
+  {
+    "revision": "d6d0631b99025b97d76423ec15503a11",
+    "url": "/hangman/static/media/hangman3.d6d0631b.svg"
+  },
+  {
+    "revision": "7681c176ca9e22f62eb4029de8fe57a4",
+    "url": "/hangman/static/media/hangman4.7681c176.svg"
+  },
+  {
+    "revision": "e16ca25810071c7e13989b9021202034",
+    "url": "/hangman/static/media/hangman5.e16ca258.svg"
+  },
+  {
+    "revision": "436da02aa58b47642e10ba775b9c397d",
+    "url": "/hangman/static/media/hangman6.436da02a.svg"
+  },
+  {
+    "revision": "36a92f20bb22678f234d3913acf14b52",
+    "url": "/hangman/static/media/hangman7.36a92f20.svg"
+  },
+  {
+    "revision": "eb5eb69bee19de4734d3f2cfd38b4251",
+    "url": "/hangman/static/media/hangman8.eb5eb69b.svg"
+  },
+  {
+    "revision": "037dc3e191672ed65d23b3167c09a742",
+    "url": "/hangman/static/media/hangman9.037dc3e1.svg"
+  }
+]);
