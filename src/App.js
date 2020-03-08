6f70326cc607982b41eadd258424eb1cee3e75da
Courtney-Watts <courtney@siteglide.com>
2020-03-08 16:04:52 +0000

    responsive placeholder text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Hangman from './components/Hangman'
 function App () {
   return (
     <div className='App'>
+      <div className='small-screen-text d-flex-col text-center p-5'>
+        <p>Browser window is too small</p>
+        <p className='pt-4'>Please open on a desktop browser</p>
+        <p className='pt-4'> If you are using a desktop browser, please resize your browser window</p>
+      </div>
       <Hangman />
     </div>
   )
